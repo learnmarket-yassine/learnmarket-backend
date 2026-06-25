ff277635c3e6edda1731cedc0d9769cb7a87fc99
yassin-ben-haj-ali <46385684+yassin-ben-haj-ali@users.noreply.github.com>
2026-06-25 12:32:25 +0200

    Fix cicd pipline (#29)
    * fix cicd pipline and create multi staged environments
    
    * fix docker compose command
    
    * add public decorator to health controller

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { HealthService, type HealthStatus } from './health.service';
+import { Public } from '../common/decorators/public.decorator';
 
 @Controller('health')
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
+  @Public()
   @Get()
   @HttpCode(HttpStatus.OK)
   async check(): Promise<HealthStatus> {
